4c667236225d5e79cb453d66de86a65b6429f597
Alexey Protasov <maleventum@gmail.com>
2020-06-09 23:03:41 +0300

    Add build files

---
+++ precache-manifest.8af47dc8e0134010f041cde0b9c2ffeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4903b5c9fa25c398207ba141b024e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562950d0b144419972b0",
+    "url": "/static/css/2.8fa4a888.chunk.css"
+  },
+  {
+    "revision": "93848f3be8b6069cd3d7",
+    "url": "/static/css/main.1e2061c5.chunk.css"
+  },
+  {
+    "revision": "562950d0b144419972b0",
+    "url": "/static/js/2.e404cc34.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.e404cc34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93848f3be8b6069cd3d7",
+    "url": "/static/js/main.5ddf1ac2.chunk.js"
+  },
+  {
+    "revision": "2ae4af333f6b6192bf8d",
+    "url": "/static/js/runtime-main.22a8c98e.js"
+  },
+  {
+    "revision": "a9ee8a2667383422e06610e692556283",
+    "url": "/static/media/ape.a9ee8a26.jpg"
+  }
+]);
